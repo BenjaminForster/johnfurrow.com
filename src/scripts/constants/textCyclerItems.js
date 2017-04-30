d0057b3a8e36431ea98b0905f49ce5bdbd7560fc
John Furrow <johnf@mesosphere.io>
2017-04-30 10:34:15 -0700

    Update copy

--- src/scripts/constants/textCyclerItems.js
+++ src/scripts/constants/textCyclerItems.js
@@ -1,11 +1,9 @@
 module.exports = [
   'building user interfaces',
-  'React',
+  'animating SVGs',
   'writing JavaScript',
-  'foggy days in SF',
-  'the BEM methodology',
-  'designing stuff',
-  'writing CSS',
-  'puppies',
-  'ES6'
+  'data visualization',
+  'good design',
+  'React',
+  'writing CSS (well, SCSS)'
 ];
